aa2145ca8d1b16c52843ac4acdd57c026240a93e
Joey Hills <joeymhills@gmail.com>
2023-04-16 19:17:15 -0400

    big moves

--- src/App.js
+++ src/App.js
@@ -1,25 +1,100 @@
-import logo from './logo.svg';
 import './App.css';
+import headshot from './pics/headshot.jpeg'
+import Component from 'react'
+import React,{useEffect, useState} from 'react'
+import clearlogo from './pics/alt-logo.png'
+import mobilelogo from './pics/JMHlogo.png'
+import coding from './icons/coding.png'
+import responsive from './icons/responsive.png'
+import animation from './icons/animation.png'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app">
+      <head>
+        <link rel="stylesheet" type="text/css" href="style.css" /> 
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" 
+        integrity="sha512-NhSC1YmyruXifcj/KFRWoC561YpHpc5Jtzgvbuzx5VozKpWvQ+4nXhPdFgmx8xqexRcpAglTj9sIBWINXa8x5w==" 
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" 
+        integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" 
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+  
+        <title>JMH Web Design</title>
+      </head>
+      
+      <header className="app-header">
+         <div className= "header-logo">
+            <img src = {mobilelogo} className= "JMHlogo" alt="" />
+         </div>
+         
+        <div className="Hambutton">
+         <i class="fa-solid fa-bars hamb"></i>
+        </div>
       </header>
+      
+      <div className="homepage-wrapper">
+        <div className="flex-box2">
+          <h2>Make your business stand out</h2>
+          <p>with a website custom made for your needs.</p>
+        </div>
+      </div>
+      <div className="services-page">
+        <h1 className="services-heading">Services</h1>
+        
+        <div className='icon-wrapper'>
+            <div className="customCoded">
+              <h2>Custom Coded</h2>
+              <img src={coding} className='icons' alt='' />
+              <p>Custom coded to look unique and profesional</p>
+            </div>
+            <div className="responsive">
+              <h2>Responsive</h2>
+              <img src={responsive} className='icons' alt='' />
+              <p>Built responsively to look great on all devices</p>
+            </div>
+            <div className="animated">
+              <h2>Animated</h2>
+              <img src={animation} className='icons' alt='' />
+              <p>Animations to create a better user experience</p>
+            </div>
+            <div className="serviceButtonWrap" href=''>
+              <button className="servicesButton">Learn more</button>
+            </div>
+        </div>
+        
+
+      </div>
+      <div className="about-page">
+        <div className="about-heading">
+          <h1>About</h1>
+        </div>
+        <img src={headshot} className="headshot" alt = " " />
+        <div className='about-heading'>
+          <h2>Who am I?</h2>
+        </div>
+        <p className="about-p">
+          I'm Joey Hills, a full-stack web developer from Milford, Connecticut. I specialize in working with businesses
+        to make fast, responsive, and profesional websites.
+        </p>
+        <p className="about-p">
+            I'm profecient in HTML, CSS, JavaScript, and Python, along with
+          popular frameworks these languages use such as React and Django.
+        </p>
+      </div>
+      <footer className="footer">
+        <ul className='footerList'>
+          <li className='footerItems'>Phone: 203-361-8525</li>
+          <li className='footerItems'>Email: Joeymhills@gmail.com</li>
+          <li className='footerItems'>Copyright 2023</li>
+        </ul>
+      </footer>
     </div>
   );
 }
+
 
 export default App;
